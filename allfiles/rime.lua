90038ea519889d8fb882268ffca32ac277da1ae9
oniondelta <54584047+oniondelta@users.noreply.github.com>
2021-07-31 03:55:48 +0800

    Add files via upload

--- allfiles/rime.lua
+++ allfiles/rime.lua
@@ -351,29 +351,70 @@ end
 --[[
 行列30空碼'⎔'轉成不輸出任何符號，符合原生
 --]]
+
+-- preedit_format 格式轉寫
+local function xform_array30_input(ainput)
+  if ainput == "" then return "" end
+  ainput = string.gsub(ainput, "a", "1-")
+  ainput = string.gsub(ainput, "b", "5⇣")
+  ainput = string.gsub(ainput, "c", "3⇣")
+  ainput = string.gsub(ainput, "d", "3-")
+  ainput = string.gsub(ainput, "e", "3⇡")
+  ainput = string.gsub(ainput, "f", "4-")
+  ainput = string.gsub(ainput, "g", "5-")
+  ainput = string.gsub(ainput, "h", "6-")
+  ainput = string.gsub(ainput, "i", "8⇡")
+  ainput = string.gsub(ainput, "j", "7-")
+  ainput = string.gsub(ainput, "k", "8-")
+  ainput = string.gsub(ainput, "l", "9-")
+  ainput = string.gsub(ainput, "m", "7⇣")
+  ainput = string.gsub(ainput, "n", "6⇣")
+  ainput = string.gsub(ainput, "o", "9⇡")
+  ainput = string.gsub(ainput, "p", "0⇡")
+  ainput = string.gsub(ainput, "q", "1⇡")
+  ainput = string.gsub(ainput, "r", "4⇡")
+  ainput = string.gsub(ainput, "s", "2-")
+  ainput = string.gsub(ainput, "t", "5⇡")
+  ainput = string.gsub(ainput, "u", "7⇡")
+  ainput = string.gsub(ainput, "v", "4⇣")
+  ainput = string.gsub(ainput, "w", "2⇡")
+  ainput = string.gsub(ainput, "x", "2⇣")
+  ainput = string.gsub(ainput, "y", "6⇡")
+  ainput = string.gsub(ainput, "z", "1⇣")
+  ainput = string.gsub(ainput, "%.", "9⇣")
+  ainput = string.gsub(ainput, "/", "0⇣")
+  ainput = string.gsub(ainput, ";", "0-")
+  ainput = string.gsub(ainput, ",", "8⇣")
+  ainput = string.gsub(ainput, "% ", "▫")
+  ainput = string.gsub(ainput, "^==", "《查注音》")
+  return ainput
+end
+
 function array30_nil_filter(input, env)
   for cand in input:iter() do
     if (string.find(cand.text, '^⎔%d$' )) then
       -- local cccc = string.gsub(cand.text, "^⎔2$", "⎔")
-      -- local cand = Candidate('date', seg.start, seg._end, '@@@', '@@@@')
-      -- cand.preedit = input .. '@'
-      -- cand_uci_m.preedit = "'/e " .. uc_i
       -- cand.text = '⎔'
       -- cand:get_genuine().text = '@'
       -- cand:get_genuine().comment = cand.comment .. "⎔"
-      local commit = env.engine.context:get_commit_text()
 --[[
       欲定義的 translator 包含三個輸入參數：
       - input: 待翻譯的字符串
       - seg: 包含 `start` 和 `_end` 兩個屬性，分別表示當前串在輸入框中的起始和結束位置
       - env: 可選參數，表示 translator 所處的環境
 --]]
-      -- local text = cand.text
       -- yield(Candidate("date", seg.start, seg._end, "⎔" , "〔日期〕"))
       -- yield(Candidate("date", seg.start, seg._end, string.gsub(cand.text, "^⎔2$", "⎔") , "〔日期〕"))
+
+      -- local commit = env.engine.context:get_commit_text() -- 原版樣式
+      -- local text = cand.text -- 原版樣式
       -- yield(Candidate("cap", 0, string.len(commit) , text, cand.comment)) -- 原版樣式
-      -- yield(Candidate("array30nil", 0, string.len(commit) , "", "⎔")) -- 選擇空碼"⎔"效果為卡住
-      yield(Candidate("array30nil", 0, 2 , "", "⎔")) -- 選擇空碼"⎔"效果為取消 -- 測試string.len('⎔')等於3
+
+      local array30_input = env.engine.context.input
+      local array30_nil_cand = Candidate("array30nil", 0, string.len(array30_input) , "", "⎔") -- 選擇空碼"⎔"效果為取消 -- 測試string.len('⎔')等於「3」，如設置「4」為==反查時就不會露出原英文編碼
+      -- local array30_nil_cand = Candidate("array30nil", 0, string.len(commit) , "", "⎔") -- 選擇空碼"⎔"效果為卡住，但 preedit 顯示會有問題
+      array30_nil_cand.preedit = xform_array30_input(array30_input)
+      yield(array30_nil_cand)
     else
       yield(cand)
     end
