1f2f2f7eab1520a8eda8f21e7334b0443acaaaba
oniondelta <deltazonetw@gmail.com>
2022-10-09 04:48:56 +0800

    修

--- allfiles/lua/processor_mobile_bpmf.lua
+++ allfiles/lua/processor_mobile_bpmf.lua
@@ -8,11 +8,11 @@ local function mobile_bpmf(key, env)
   local context = engine.context
   local input_m = context.input
   local orig_m = context:get_commit_text()
-  if context:get_option('ascii_mode') then
-    return 2
-  elseif (not context:is_composing()) then
-    return 2
-  elseif (key:repr() == "space") then
+  -- if context:get_option('ascii_mode') then
+  --   return 2
+  -- elseif (not context:is_composing()) then
+  --   return 2
+  if (key:repr() == "space") and (context:is_composing()) then
     -- local input_m = context.input
     if (string.find(input_m, "^[a-z][-_.0-9a-z]*@.*$")) or (string.find(input_m, "^https?:.*$")) or (string.find(input_m, "^ftp:.*$")) or (string.find(input_m, "^mailto:.*$")) or (string.find(input_m, "^file:.*$")) then
     -- if ( string.find(input_m, "[@:]")) then
