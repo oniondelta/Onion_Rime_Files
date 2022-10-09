962c0f3a10f4a71bdc6eeb643fc54396f8d068b6
oniondelta <deltazonetw@gmail.com>
2022-10-09 15:14:40 +0800

    修

--- allfiles/lua/processor_array30up_mix.lua
+++ allfiles/lua/processor_array30up_mix.lua
@@ -11,10 +11,10 @@ local function array30up_mix(key, env)
   local orig_array = context:get_commit_text()
   if context:get_option('ascii_mode') then
     return 2
-  elseif (not context:has_menu()) then
-    return 2
-  elseif (key:repr() == "space") then
-  -- if (key:repr() == "space") and (context:has_menu()) then
+  -- elseif (not context:has_menu()) then
+  --   return 2
+  elseif (key:repr() == "space") and (context:has_menu()) then
+  -- if (key:repr() == "space") then
   -- if (key:repr() == "space") and (context:is_composing()) then
     -- local input_array = context.input
     if (string.find(input_array, "^[a-z.,/;][a-z.,/;][a-z.,/;][a-z.,/;]?i?$")) or (string.find(input_array, "^==[a-z.,/;][a-z.,/;][a-z.,/;][a-z.,/;]?i?$")) or (string.find(input_array, "`.+$")) or (string.find(input_array, "^[a-z][-_.0-9a-z]*@.*$")) or (string.find(input_array, "^https?:.*$")) or (string.find(input_array, "^ftp:.*$")) or (string.find(input_array, "^mailto:.*$")) or (string.find(input_array, "^file:.*$")) or (string.find(input_array, "^=[a-z0-9,.;/-]+$")) then
@@ -24,8 +24,8 @@ local function array30up_mix(key, env)
       context:clear()
       return 1 -- kAccepted
     end
-  elseif (key:repr() == "Return") then
-  -- elseif (key:repr() == "Return") and (context:has_menu()) then
+  elseif (key:repr() == "Return") and (context:has_menu()) then
+  -- elseif (key:repr() == "Return") then
   -- elseif (key:repr() == "Return") and (context:is_composing()) then
     -- local input_array = context.input
     if (string.find(input_array, "^=[a-z0-9,.;/-]+$")) then
