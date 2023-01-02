ebb35860ff9582dc3af3c850abdeeb6f903b9039
oniondelta <deltazonetw@gmail.com>
2023-01-03 02:51:11 +0800

    修

--- allfiles/lua/translator_convert_japan_translator.lua
+++ allfiles/lua/translator_convert_japan_translator.lua
@@ -699,9 +699,11 @@ local function convert_japan_translator(input, seg)
 end
 
 -- 掛接方案用，掛接方案名稱「japan」。
-local function p_convert_japan_translator(input, seg, env)
-  local tips_jp = env.engine.schema.config:get_string('japan/tips')
-  local tips_jp = string.gsub(tips_jp, "▶", "")
+local function p_convert_japan_translator(input, seg)
+-- local function p_convert_japan_translator(input, seg, env)
+  -- local tips_jp = env.engine.schema.config:get_string('japan/tips')
+  -- local tips_jp = string.gsub(tips_jp, "▶", "")
+  local tips_jp = '《日系列》'
   local c = string.match(input, "([-/a-z.,;]+)[.][,]$")
   local tag_jp = seg:has_tag("japan")
   if tag_jp and (c~=nil) then
